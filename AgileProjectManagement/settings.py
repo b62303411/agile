be85351d03162488fc606e1107ef7e46e6575e81
b62303411 <samuel.ars@gmail.com>
2023-07-09 16:47:53 -0400

    test

--- AgileProjectManagement/settings.py
+++ AgileProjectManagement/settings.py
@@ -40,5 +40,14 @@ LOGGING = {
 
 ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'AgileProjectManagement.urls'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 logging.info('Settings loaded successfully.')
