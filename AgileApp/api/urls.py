41e1b74f79c2b657f1c0ecddcc717b5cc0552f1c
b62303411 <samuel.ars@gmail.com>
2023-07-09 16:35:09 -0400

    test

--- AgileApp/api/urls.py
+++ AgileApp/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+import os
+import sys
+import logging
 urlpatterns = [
     path('users/', views.user_list, name='user_list'),
     path('initiatives/', views.initiative_list, name='initiative_list'),
@@ -11,3 +13,10 @@ urlpatterns = [
     path('userstories/', views.userstory_list, name='userstory_list'),
     #path('api/', include('AgileApp.api.urls')),
 ]
+# Log available URL paths
+for url in urlpatterns:
+    if hasattr(url, 'url_patterns'):
+        for included_url in url.url_patterns:
+            logging.info(f"URL pattern: {included_url.pattern}")
+    else:
+        logging.info(f"URL pattern: {url.pattern}")
