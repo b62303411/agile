16399b01460b3c36e6961b3a9f046ded02aa5285
b62303411 <samuel.ars@gmail.com>
2023-07-09 16:23:28 -0400

    test

---
+++ AgileApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.user_list, name='user_list'),
+    path('initiatives/', views.initiative_list, name='initiative_list'),
+    path('epics/', views.epic_list, name='epic_list'),
+    path('projects/', views.project_list, name='project_list'),
+    path('sprints/', views.sprint_list, name='sprint_list'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('userstories/', views.userstory_list, name='userstory_list'),
+]
