413cf1218d760c3e6a06ef2e6221e667602a5aa7
b62303411 <samuel.ars@gmail.com>
2023-07-09 18:22:46 -0400

    test

---
+++ AgileApp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import User, Initiative, Epic, Project, Sprint, Task, UserStory
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Initiative)
+admin.site.register(Epic)
+admin.site.register(Project)
+admin.site.register(Sprint)
+admin.site.register(Task)
+admin.site.register(UserStory)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined')
+
+admin.site.register(User, UserAdmin)
