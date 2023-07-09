0a7d219fb6b738d2ab0d4938864ef376e83fca7b
b62303411 <samuel.ars@gmail.com>
2023-07-09 12:06:32 -0400

    test

--- AgileProjectManagement/settings.py
+++ AgileProjectManagement/settings.py
@@ -8,4 +8,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
+
 ALLOWED_HOSTS = ['*']
