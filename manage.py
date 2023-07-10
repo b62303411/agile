b200e3b5b9ba5231f2bda1ac56a4b768110244b4
samuel.audet-arsenault@coldchainscience.com <samuel.audet-arsenault@coldchainscience.com>
2023-07-10 07:58:20 -0400

    let see if it works

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 import logging
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgileProjectManagement.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agile.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +18,7 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     logging.info('Starting Django application...')
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
