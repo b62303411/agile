b699b3f997a5f196cfab5c0e9df72b360087cbd3
b62303411 <samuel.ars@gmail.com>
2023-07-10 09:44:49 -0400

    test

--- agile/views.py
+++ agile/views.py
@@ -8,30 +8,30 @@ logging.info("loading views ..")
 def user_list(request):
     logging.info(f"user_list: {request}")
     users = User.objects.all()
-    return render(request, 'AgileApp/user_list.html', {'users': users})
+    return render(request, 'AgileApp/templates/user_list.html', {'users': users})
 
 def initiative_list(request):
     logging.info(f"initiative_list: {request}")
     initiatives = Initiative.objects.all()
-    return render(request, 'AgileApp/initiative_list.html', {'initiatives': initiatives})
+    return render(request, 'AgileApp/templates/initiative_list.html', {'initiatives': initiatives})
 
 def epic_list(request):
     logging.info(f"epic_list: {request}")
     epics = Epic.objects.all()
-    return render(request, 'AgileApp/epic_list.html', {'epics': epics})
+    return render(request, 'AgileApp/templates/epic_list.html', {'epics': epics})
 
 def project_list(request):
     projects = Project.objects.all()
-    return render(request, 'AgileApp/project_list.html', {'projects': projects})
+    return render(request, 'AgileApp/templates/project_list.html', {'projects': projects})
 
 def sprint_list(request):
     sprints = Sprint.objects.all()
-    return render(request, 'AgileApp/sprint_list.html', {'sprints': sprints})
+    return render(request, 'AgileApp/templates/sprint_list.html', {'sprints': sprints})
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'AgileApp/task_list.html', {'tasks': tasks})
+    return render(request, 'AgileApp/templates/task_list.html', {'tasks': tasks})
 
 def userstory_list(request):
     userstories = UserStory.objects.all()
-    return render(request, 'AgileApp/userstory_list.html', {'userstories': userstories})
+    return render(request, 'AgileApp/templates/userstory_list.html', {'userstories': userstories})
