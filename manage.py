b1849826c2d79f7069d79fe3afb39235167967ac
samuel.audet-arsenault@coldchainscience.com <samuel.audet-arsenault@coldchainscience.com>
2023-07-10 08:44:33 -0400

    test

--- manage.py
+++ manage.py
@@ -2,11 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import logging
+
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agile.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +14,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info('Starting Django application...')
     execute_from_command_line(sys.argv)
 
 
